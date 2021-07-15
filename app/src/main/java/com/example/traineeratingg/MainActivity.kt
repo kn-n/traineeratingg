f14e07a129ad3676d90d01ce4b42062c4febc589
komle <k_nastya3@mail.ru>
2021-07-15 18:32:40 +0500

    header name

--- app/src/main/java/com/example/traineeratingg/MainActivity.kt
+++ app/src/main/java/com/example/traineeratingg/MainActivity.kt
@@ -6,22 +6,39 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.TextView
+import com.example.traineeratingg.Data.Firebase.CURRENT_USER
+import com.example.traineeratingg.Data.Firebase.NODE_USERS
+import com.example.traineeratingg.Data.Firebase.REF_DATABASE_ROOT
+import com.example.traineeratingg.Data.Firebase.initFirebase
 
 private val teamFragment = TeamFragment()
 private val userAnalyticFragment = UserAnalyticFragment()
 private val accountFragment = AccountFragment()
 
 class MainActivity : AppCompatActivity() {
+    private val nameOfFragment = findViewById<TextView>(R.id.name_of_activity)
+    private val navAnalytics = findViewById<ImageButton>(R.id.analytics)
+    private val navTeams = findViewById<ImageButton>(R.id.teams)
+    private val navAcc = findViewById<ImageButton>(R.id.acc)
+    private val back = findViewById<ImageButton>(R.id.back)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navAnalytics = findViewById<ImageButton>(R.id.analytics)
         navAnalytics.isSelected = false
-        val navTeams = findViewById<ImageButton>(R.id.teams)
         navTeams.isSelected  = true
-        val navAcc = findViewById<ImageButton>(R.id.acc)
         navAcc.isSelected = false
+
+        back.visibility=View.GONE
+
+        initFirebase()
+        REF_DATABASE_ROOT.child(NODE_USERS).child(CURRENT_USER).addValueEventListener(
+                AppValueEventListener{
+                    val user = it.getValue(User::class.java)
+                    nameOfFragment.text = user!!.team
+                }
+        )
 
         val fragmentManager = supportFragmentManager.beginTransaction()
         fragmentManager.add(R.id.frame, teamFragment)
@@ -31,31 +48,34 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
 
-        val navAnalytics = findViewById<ImageButton>(R.id.analytics)
         navAnalytics.isSelected = false
-        val navTeams = findViewById<ImageButton>(R.id.teams)
         navTeams.isSelected  = true
-        val navAcc = findViewById<ImageButton>(R.id.acc)
         navAcc.isSelected = false
 
         navAnalytics.setOnClickListener {
+            nameOfFragment.text = "Анализ"
             commitFragment(navAnalytics)
         }
 
         navAcc.setOnClickListener {
+            nameOfFragment.text = "Аккаунт"
             commitFragment(navAcc)
         }
 
         navTeams.setOnClickListener {
+            initFirebase()
+            REF_DATABASE_ROOT.child(NODE_USERS).child(CURRENT_USER).addValueEventListener(
+                    AppValueEventListener{
+                        val user = it.getValue(User::class.java)
+                        nameOfFragment.text = user!!.team
+                    }
+            )
             commitFragment(navTeams)
         }
 
     }
 
     fun commitFragment(view: View){
-        val navAnalytics = findViewById<ImageButton>(R.id.analytics)
-        val navTeams = findViewById<ImageButton>(R.id.teams)
-        val navAcc = findViewById<ImageButton>(R.id.acc)
         val fragmentManager = supportFragmentManager.beginTransaction()
         if (view == navAcc){
             navAnalytics.isSelected = false
