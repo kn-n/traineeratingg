5ac1bab896c4beb3731f7c34df94a684aa789f4e
komle <k_nastya3@mail.ru>
2021-07-15 18:46:08 +0500

    header name

--- app/src/main/java/com/example/traineeratingg/MainActivity.kt
+++ app/src/main/java/com/example/traineeratingg/MainActivity.kt
@@ -17,14 +17,14 @@ private val userAnalyticFragment = UserAnalyticFragment()
 private val accountFragment = AccountFragment()
 
 class MainActivity : AppCompatActivity() {
-    private val nameOfFragment = findViewById<TextView>(R.id.name_of_activity)
-    private val navAnalytics = findViewById<ImageButton>(R.id.analytics)
-    private val navTeams = findViewById<ImageButton>(R.id.teams)
-    private val navAcc = findViewById<ImageButton>(R.id.acc)
-    private val back = findViewById<ImageButton>(R.id.back)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val nameOfFragment = findViewById<TextView>(R.id.name_of_activity)
+        val navAnalytics = findViewById<ImageButton>(R.id.analytics)
+        val navTeams = findViewById<ImageButton>(R.id.teams)
+        val navAcc = findViewById<ImageButton>(R.id.acc)
+        val back = findViewById<ImageButton>(R.id.back)
 
         navAnalytics.isSelected = false
         navTeams.isSelected  = true
@@ -47,6 +47,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+        val nameOfFragment = findViewById<TextView>(R.id.name_of_activity)
+        val navAnalytics = findViewById<ImageButton>(R.id.analytics)
+        val navTeams = findViewById<ImageButton>(R.id.teams)
+        val navAcc = findViewById<ImageButton>(R.id.acc)
 
         navAnalytics.isSelected = false
         navTeams.isSelected  = true
@@ -75,29 +79,36 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun commitFragment(view: View){
+    private fun commitFragment(view: View){
+        val navAnalytics = findViewById<ImageButton>(R.id.analytics)
+        val navTeams = findViewById<ImageButton>(R.id.teams)
+        val navAcc = findViewById<ImageButton>(R.id.acc)
         val fragmentManager = supportFragmentManager.beginTransaction()
-        if (view == navAcc){
-            navAnalytics.isSelected = false
-            navTeams.isSelected  = false
-            navAcc.isSelected = true
+        when (view) {
+            navAcc -> {
+                navAnalytics.isSelected = false
+                navTeams.isSelected  = false
+                navAcc.isSelected = true
 
-            fragmentManager.replace(R.id.frame, accountFragment)
-            fragmentManager.commit()
-        }else if (view == navTeams){
-            navAnalytics.isSelected = false
-            navTeams.isSelected  = true
-            navAcc.isSelected = false
+                fragmentManager.replace(R.id.frame, accountFragment)
+                fragmentManager.commit()
+            }
+            navTeams -> {
+                navAnalytics.isSelected = false
+                navTeams.isSelected  = true
+                navAcc.isSelected = false
 
-            fragmentManager.replace(R.id.frame, teamFragment)
-            fragmentManager.commit()
-        }else if(view == navAnalytics){
-            navAnalytics.isSelected = true
-            navTeams.isSelected  = false
-            navAcc.isSelected = false
+                fragmentManager.replace(R.id.frame, teamFragment)
+                fragmentManager.commit()
+            }
+            navAnalytics -> {
+                navAnalytics.isSelected = true
+                navTeams.isSelected  = false
+                navAcc.isSelected = false
 
-            fragmentManager.replace(R.id.frame, userAnalyticFragment)
-            fragmentManager.commit()
+                fragmentManager.replace(R.id.frame, userAnalyticFragment)
+                fragmentManager.commit()
+            }
         }
     }
 }
