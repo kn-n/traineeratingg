1839442a4c63677c4ead4ef03c2520aeb7558270
lisiyytka <gritroma@yandex.ru>
2021-07-19 16:20:24 +0500

    RegisterActivity.kt and LoginActivity.kt

--- app/src/main/java/com/example/traineeratingg/UserAnalyticFragment.kt
+++ app/src/main/java/com/example/traineeratingg/UserAnalyticFragment.kt
@@ -13,6 +13,7 @@ import com.example.traineeratingg.Data.Firebase.CURRENT_USER
 import com.example.traineeratingg.Data.Firebase.NODE_TEAMS
 import com.example.traineeratingg.Data.Firebase.NODE_USERS
 import com.example.traineeratingg.Data.Firebase.REF_DATABASE_ROOT
+import com.google.android.gms.tasks.Tasks
 
 class UserAnalyticFragment: Fragment() {
 
@@ -37,6 +38,7 @@ class UserAnalyticFragment: Fragment() {
         val param4 = view?.findViewById<TextView>(R.id.param4)
         gradesRecyclerView = view?.findViewById(R.id.grades)!!
 
+
         REF_DATABASE_ROOT.child(NODE_USERS).child(CURRENT_USER).addValueEventListener(
                 AppValueEventListener{
                     val user = it.getValue(User::class.java)
@@ -50,6 +52,29 @@ class UserAnalyticFragment: Fragment() {
                             AppValueEventListener{
                                 val tasks = it.children.map { it.key }
                                 initRecyclerView(tasks as List<String>)
+                                for (task in tasks){
+                                    REF_DATABASE_ROOT.child(NODE_TEAMS).child(user.team).child(NODE_USERS).child(CURRENT_USER).child(task).addValueEventListener(
+                                            AppValueEventListener{
+                                                val grades = it.children.map { it.getValue(String::class.java) }
+                                                var p1 = 0
+                                                var p2 = 0
+                                                var p3 = 0
+                                                var p4 = 0
+                                                for (grade in grades){
+                                                    val splitGrade = grade!!.split(" ")
+                                                    p1 +=splitGrade[0].toInt()
+                                                    p2 +=splitGrade[1].toInt()
+                                                    p3 +=splitGrade[2].toInt()
+                                                    p4 +=splitGrade[3].toInt()
+                                                    context?.let { it1 -> makeToast(it1,splitGrade[3]) }
+                                                }
+                                                param1!!.text = (p1/tasks.size).toString()
+                                                param2!!.text = (p2/tasks.size).toString()
+                                                param3!!.text = (p3/tasks.size).toString()
+                                                param4!!.text = (p4/tasks.size).toString()
+                                            }
+                                    )
+                                }
                             }
                     )
                 }
@@ -82,11 +107,11 @@ class UserAnalyticFragment: Fragment() {
                                                 holder.param4.text = ((holder.param4.text.toString().toInt() + listMarks[3].toInt())/marks.size).toString()
                                             }
                                         }
-                                        REF_DATABASE_ROOT.child(NODE_TEAMS).child(user!!.team).child(NODE_USERS).child(CURRENT_USER).child(tasks[position]).child(CURRENT_USER).addValueEventListener(
+                                        REF_DATABASE_ROOT.child(NODE_TEAMS).child(user.team).child(NODE_USERS).child(CURRENT_USER).child(tasks[position]).child(CURRENT_USER).addValueEventListener(
                                                 AppValueEventListener{
                                                     val myMarks = it.getValue(String::class.java)
                                                     if (myMarks.isNullOrEmpty()){
-                                                        holder.evaluate!!.visibility = View.VISIBLE
+                                                        holder.evaluate.visibility = View.VISIBLE
                                                         holder.eventName.text = tasks[position]
                                                         holder.evaluate.setOnClickListener {
                                                             replaceFragment(EvaluateFragment(tasks[position], CURRENT_USER))
